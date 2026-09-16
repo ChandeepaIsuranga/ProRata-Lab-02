d2299deeb65f4f3374fb76caeadf0db1c82188a2
Chandeepa Isuranga <87310674+ChandeepaIsuranga@users.noreply.github.com>
2026-09-16 15:44:41 +0530

    Add Lab2Q3.java to Q3

---
+++ Q3/IT21112164Lab2Q3.java
@@ -0,0 +1,13 @@
+public class IT21112164Lab2Q3 {
+    public static void main(String[] args) {
+
+        double sideA = 3;
+        double sideB = 4;
+
+        double hypotenuse;
+
+        hypotenuse = Math.sqrt((sideA * sideA) + (sideB * sideB));
+
+        System.out.println("Length of the hypotenuse = " + hypotenuse);
+    }
+}
